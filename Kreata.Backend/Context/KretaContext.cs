4dc5700862b6ea6f485282a8ca9fc9e715be65ba
Kugesz <81580775+Kugesz@users.noreply.github.com>
2024-11-06 18:30:59 +0100

    asd

--- Kreata.Backend/Context/KretaContext.cs
+++ Kreata.Backend/Context/KretaContext.cs
@@ -1,12 +1,16 @@
 ﻿using Kreata.Backend.Datas.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace Kreata.Backend.Context
 {
     public class KretaContext : DbContext
     {
         public DbSet<Student> Students { get; set; }
-        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Parent> Parents { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Item> Items { get; set; }
 
         public KretaContext(DbContextOptions<KretaContext> options)
             : base(options)
@@ -15,6 +19,7 @@ namespace Kreata.Backend.Context
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            
             modelBuilder.Seed();
             base.OnModelCreating(modelBuilder);
         }
--- Kreata.Backend/Context/ModelBuilderExtension.cs
+++ Kreata.Backend/Context/ModelBuilderExtension.cs
@@ -8,6 +8,59 @@ namespace Kreata.Backend.Context
     {
         public static void Seed(this ModelBuilder modelBuilder)
         {
+            User user1 = new User {
+                Id = Guid.NewGuid(),
+                UserName = "hegzi",
+                UserPassword = "securePassword123",
+                FirstName = "Hegyi",
+                LastName = "Herka",
+                Email = "hegzi@example.com",
+                Company = "Example Corp",
+                Admin = false
+            };
+
+            User user2 = new User
+            {
+                Id = Guid.NewGuid(),
+                UserName = "szabi",
+                UserPassword = "anotherSecurePassword",
+                FirstName = "Szabó",
+                LastName = "Miklós",
+                Email = "szabo@example.com",
+                Company = "Another Corp",
+                Admin = true
+            };
+
+            Item item1 = new Item
+            {
+                Id = Guid.NewGuid(),
+                Name = "tej"
+            };
+
+            Item item2 = new Item
+            {
+                Id = Guid.NewGuid(),
+                Name = "wc papir"
+            };
+
+            Order order1 = new Order
+            {
+                OrderId = Guid.NewGuid(),
+                BuyerName = user1.FirstName + user1.LastName,
+                CreatedAt = DateTime.Now,
+                DeliverTo = "6750 Szeged Zágráb utca 54/2",
+                Products = item1.Name
+            };
+
+            Order order2 = new Order
+            {
+                OrderId = Guid.NewGuid(),
+                BuyerName = user2.FirstName + user2.LastName,
+                CreatedAt = DateTime.Now,
+                DeliverTo = "6720 Szeged Eszterga utca 56",
+                Products = item1.Name+","+item2.Name 
+            };
+
             List<Student> students = new List<Student>
             {
                 new Student
@@ -15,7 +68,7 @@ namespace Kreata.Backend.Context
                     Id=Guid.NewGuid(),
                     FirstName="János",
                     LastName="Jegy",
-                    BirthsDay=new DateTime(2011,10,10),
+                    BirthsDay=new DateTime(2022,10,10),
                     SchoolYear=9,
                     SchoolClass = SchoolClassType.ClassA,
                     EducationLevel="érettségi"
@@ -25,39 +78,54 @@ namespace Kreata.Backend.Context
                     Id=Guid.NewGuid(),
                     FirstName="Szonja",
                     LastName="Stréber",
-                    BirthsDay=new DateTime(2012,4,4),
+                    BirthsDay=new DateTime(2021,4,4),
                     SchoolYear=10,
                     SchoolClass = SchoolClassType.ClassB,
                     EducationLevel="érettségi"
                 }
             };
 
-            // Students
-            modelBuilder.Entity<Student>().HasData(students);
-
-            List<Teacher> techers = new List<Teacher>
+            List<Parent> parents = new List<Parent>
             {
-                new Teacher
+                new Parent
                 {
                     Id=Guid.NewGuid(),
-                    FirstName="Feleltető",
-                    LastName="Ferenc",
-                    BirthsDay=new DateTime(2001,8,1),
-                    IsWoomen=false,
-                    IsHeadTeacher=false
+                    FirstName="Hegyi",
+                    LastName="Herka",
+                    Address="6750 Szeged Zágráb utca 54/2",
+                    IsWoomen = false,
                 },
-                new Teacher
+                new Parent
                 {
                     Id=Guid.NewGuid(),
-                    FirstName="Aranyos",
-                    LastName="Aranka",
-                    BirthsDay=new DateTime(2002,2,24),
-                    IsWoomen=true,
-                    IsHeadTeacher=true
+                    FirstName="Illés",
+                    LastName="Ákos",
+                    Address="6850 Algyő Bartók Béla utca 59",
+                    IsWoomen = true,
                 }
             };
 
-            modelBuilder.Entity<Teacher>().HasData(techers);
+            List<Order> orders = new List<Order>
+            {
+                order1, order2
+            };
+
+            List<User> users = new List<User>
+            {
+                user1,user2
+            };
+
+            List<Item> items = new List<Item>
+            {
+                item1,item2
+            };
+
+            // Students
+            modelBuilder.Entity<Student>().HasData(students);
+            modelBuilder.Entity<Parent>().HasData(parents);
+            modelBuilder.Entity<Order>().HasData(orders);
+            modelBuilder.Entity<User>().HasData(users);
+            modelBuilder.Entity<Item>().HasData(items);
         }
     }
 }
